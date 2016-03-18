f39572483e53be37593ac195fcc375f7e331ccfe
LaoSan Zhang <zerozhp@qq.com>
2016-03-18 23:38:02 +0800

    段落 客户端模版
    一个组件分部分使用
    json数据接收ajax调用结果

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var fortune = require('./lib/fortune.js');
 var app = express();
 
 //设置handlebars视图引擎
-var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -20,57 +29,69 @@ app.use(function(req, res, next){
 
 //weather
 function getWeatherData(){
-	return {
-		locations: [
-		   {
-			   name: 'Portland',
-			   forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
-			   iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
-			   weather: 'Overcast',
-			   temp: '54.1 F (12.3 C)', 
-		   },
-		   {
-			   name: 'Bend',
-			   forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
-			   iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
-			   weather: 'Partly Cloudy',
-			   temp: '55.0 F (12.8 C)', 
-		   },
-		   {
-			   name: 'Manzanita',
-			   forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
-			   iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
-			   weather: 'Light Rain',
-			   temp: '55.0 F (12.8 C)', 
-		   },
-		],
-	};
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
 }
 
 app.use(function(req, res, next){
 	if(!res.locals.partials) res.locals.partials = {};
-	res.locals.partials.weather = getWeatherData();
-	next();
+ 	res.locals.partials.weatherContext = getWeatherData();
+ 	next();
 });
 
 app.get('/',function(req, res){
 	res.render('home');
 });
-
 app.get('/about',function(req, res){
 	res.render('about', {
 		fortune: fortune.getFortune(),
 		pageTestScript: '/qa/tests-about.js'
 	});
 });
-
 app.get('/tours/hood-river',function(req, res){
 	res.render('tours/hood-river');
 });
-
 app.get('/tours/request-group-rate',function(req, res){
 	res.render('tours/request-group-rate');
 });
+app.get('/jquerytest', function(req, res){
+	res.render('jquerytest');
+});
+app.get('/nursery-rhyme', function(req, res){
+	res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+	res.json({
+		animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+	});
+});
+
 //查看浏览器发送的信息
 app.get('/headers',function(req, res){
 	res.type('text/plain');
