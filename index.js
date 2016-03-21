535e182e98278ee6ba0dcc00202d65ae1b10b7a7
zerozhp <zerozhp@qq.com>
2016-03-21 15:06:40 +0800

    文件上传

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
 
 var app = express();
 
@@ -113,7 +114,24 @@ app.post('/process2', function(req, res){
 app.get('/thank-you', function(req, res){
 	res.render('thank-you');
 });
-
+app.get('/contest/vacation-photo', function(req, res){
+	var now = new Date();
+	res.render('contest/vacation-photo',{
+		year: now.getFullYear(),
+		month: now.getMonth()
+	});
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files){
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files:');
+		console.log(files);
+		res.redirect(303, '/thank-you')
+	})
+});
 //查看浏览器发送的信息
 app.get('/headers',function(req, res){
 	res.type('text/plain');
